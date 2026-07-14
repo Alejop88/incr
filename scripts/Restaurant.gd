ac30ce4615344158f33b471188b2a1d1b39e3291
Carles <sis.tec@lacamara.es>
2026-07-14 11:03:43 +0200

    Arreglat, el pnj ja es mou a la CUINA, el fallo era Mouse Filter del HUF estava en STOP i te que estar amb ignore i elimina addon

--- scripts/Restaurant.gd
+++ scripts/Restaurant.gd
@@ -9,18 +9,53 @@ signal customer_paid(amount: float)
 var base_plate_price: float = 5.0
 
 func _ready() -> void:
+	get_viewport().physics_object_picking = true
+
+	player_waiter.input_pickable = false
+
+	print("Waiter pickable: ", player_waiter.input_pickable)
+	print("Kitchen pickable: ", kitchen_point.input_pickable)
+	print("Kitchen layer: ", kitchen_point.collision_layer)
+
 	kitchen_point.input_event.connect(_on_kitchen_point_input_event)
 	table_01_point.input_event.connect(_on_table_01_point_input_event)
 
 func serve_test_customer() -> void:
 	customer_paid.emit(base_plate_price)
 	
-func _on_kitchen_point_input_event(_viewport, event, _shape_idx) -> void:
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		print("He pulsado la cocina")
+func _on_kitchen_point_input_event(
+	_viewport: Viewport,
+	event: InputEvent,
+	_shape_idx: int
+) -> void:
+	print("KITCHEN RECIBE EVENTO: ", event)
+
+	if event is InputEventMouseButton \
+			and event.button_index == MOUSE_BUTTON_LEFT \
+			and event.pressed:
+		print("Clic izquierdo en cocina")
 		player_waiter.move_to_position(kitchen_point.global_position)
 
 func _on_table_01_point_input_event(_viewport, event, _shape_idx) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		print("Click en mesa")
 		player_waiter.move_to_position(table_01_point.global_position)
+		
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		print(
+			"GLOBAL -> botón: ",
+			event.button_index,
+			" pulsado: ",
+			event.pressed,
+			" posición: ",
+			event.position
+		)
+
+	if event is InputEventScreenTouch:
+		print(
+			"TOUCH -> pulsado: ",
+			event.pressed,
+			" posición: ",
+			event.position
+		)
